001c9a284114d3c46f94eb45afc0d6bd0325286f
Richard Hao <ithaoxilu@gmail.com>
2020-05-01 00:50:26 +0800

    Using session_id to isolate browsers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_session_id
+
+  private
+
+  def set_session_id
+    cookies.encrypted[:session_id] = session.id
+  end
 end
